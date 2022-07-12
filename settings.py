4cc58b8a58967948ef03ea6022b031e9b6c0d07d
Evgeniy112 <you@example.com>
2022-07-12 21:31:58 +0300

    parametrize

---
+++ settings.py
@@ -0,0 +1,8 @@
+valid_email = "testqatestovich@yandex.ru"
+valid_password = "mgX-HCu-s5Q-8b2"
+
+invalid_email1 = "invalidtest@yandex.ru"
+invalid_password1 = "mgX-HCu-s5Q-fgr"
+
+invalid_email2 = ""
+invalid_password2 = ""
